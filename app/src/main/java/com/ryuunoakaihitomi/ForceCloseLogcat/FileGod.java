fe889cdd8cfa892dfab3543a619dddff098192c4
ryuunoakaihitomi <18475860917@163.com>
2016-12-06 08:01:25 -0600

    Update FileGod.java

--- app/src/main/java/com/ryuunoakaihitomi/ForceCloseLogcat/FileGod.java
+++ app/src/main/java/com/ryuunoakaihitomi/ForceCloseLogcat/FileGod.java
@@ -20,11 +20,8 @@ public class FileGod
 				dir.mkdirs();
 			}
 			FileOutputStream fos = new FileOutputStream(file);  
-
 			byte [] bytes = write_str.getBytes(); 
-
 			fos.write(bytes); 
-
 			fos.close();
 		}
 		catch (IOException e)
@@ -36,17 +33,40 @@ public class FileGod
         try
 		{
 			File file = new File(fileName);  
-
 			FileInputStream fis = new FileInputStream(file);  
-
 			int length = fis.available(); 
-
 			byte [] buffer = new byte[length]; 
 			fis.read(buffer);     
-
 			res = EncodingUtils.getString(buffer, "UTF-8"); 
-
 			fis.close();     
+		}
+		catch (IOException e)
+		{}  
+		return res;
+	}  
+	private static void delete(File f)
+	{   
+		if (f.isFile())
+		{  
+			f.delete();  
+			return;  
+		}  
+		if (f.isDirectory())
+		{  
+			File[] childFiles = f.listFiles();  
+			if (childFiles == null || childFiles.length == 0)
+			{  
+				f.delete();  
+				return;  
+			}  
+			for (int i = 0; i < childFiles.length; i++)
+			{  
+				delete(childFiles[i]);  
+			}  
+			f.delete();  
+		}  
+	} 
+}
 
 		}
 		catch (IOException e)
